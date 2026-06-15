37990d56a282bf6a7db8df1f231426ac1b2540f5
DoBinhMinh <dominhptit12@gmail.com>
2026-06-15 21:16:49 +0700

    update code

--- SourceCode/backend/index.ts
+++ SourceCode/backend/index.ts
@@ -17,7 +17,7 @@ connectDB();
 
 // Cấu hình CORS
 app.use(cors({
-  origin: "https://du-an-web-tuyen-dung-6.onrender.com", // Phải chỉ định tên miền cụ thể
+  origin: "http://localhost:3000", // Phải chỉ định tên miền cụ thể
   credentials: true, // Cho phép gửi cookie
 }));
 
